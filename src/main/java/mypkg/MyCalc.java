1e3c69b7783b5fbc36fda799a6d005702ee48f3b
kalleshgit <kalleshrudrappamail@gmail.com>
2025-01-10 11:57:21 +0530

    First commit

---
+++ src/main/java/mypkg/MyCalc.java
@@ -0,0 +1,32 @@
+package mypkg;
+
+public class MyCalc {
+	
+	int sum (int a, int b)
+	{
+		return a+b;
+	}
+	int diff (int a, int b)
+	{
+		return a-b;
+	}
+	int mul (int a, int b)
+	{
+		return a*b;
+	}
+	int div (int a, int b)
+	{
+		return a/b;
+	}
+
+	public static void main(String[] args) {
+		MyCalc calc= new MyCalc();
+		System.out.println("Sum is : "+calc.sum(20, 10));
+		System.out.println("Diff is : "+calc.diff(20, 10));
+		System.out.println("Multply is : "+calc.mul(20, 10));
+		System.out.println("division is : "+calc.div(20, 10));
+
+	}
+
+}
+
